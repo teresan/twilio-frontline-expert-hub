cf588a9e028c82dd9473b02cd77b54a256bc747b
Teresa N <teresa.raquel@gmail.com>
2021-08-11 13:34:18 +0100

    avatar for inbound convo

--- serverless/frontline-service/functions/pre-event.js
+++ serverless/frontline-service/functions/pre-event.js
@@ -1,18 +1,21 @@
-exports.handler =  function (context, event, callback) {
-    console.log("PreEvent "+ JSON.stringify(event));
+exports.handler = function (context, event, callback) {
+    console.log("PreEvent " + JSON.stringify(event));
     const crm = require(Runtime.getFunctions()['crm'].path);
     console.log(event['MessagingBinding.Address']);
 
-    if(event['MessagingBinding.Address']){
-    crm.fetch(event['MessagingBinding.Address'], context.DB_URL).then(participant => { 
-        console.log(participant);
-        callback(null, { friendly_name: participant.display_name });
+    if (event['MessagingBinding.Address']) {
+        crm.fetch(event['MessagingBinding.Address'], context.DB_URL).then(participant => {
+            console.log(`participant ${participant}`);
+            callback(null, {
+                friendly_name: participant.display_name,
+                attributes: JSON.stringify({ avatar: participant.avatar })
+            });
 
-    }).catch(e => {
-        console.log(e);
-        callback("this is an error "+ e);
- 
-    });
+        }).catch(e => {
+            console.log(e);
+            callback("this is an error " + e);
+
+        });
 
     }
     else
