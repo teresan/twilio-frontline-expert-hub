1c68cdfcad4d58d1d486898b511ef5fbe512112a
aandresdelvalle <aandresdelvalle@twilio.com>
2021-09-29 18:50:15 +0100

    introduce the system participant

--- serverless/frontline-service/functions/pre-event.js
+++ serverless/frontline-service/functions/pre-event.js
@@ -28,12 +28,73 @@ exports.handler = async function (context, event, callback) {
         .messages
         .create({ author: 'system', body: 'We add here info from the BOT - but at this stage is still seen by the agent - it is routed' }); //pass the person
        
+        if(event['Body']=='route'){
+
+
+            let selectedWorker = await getLastFrontLineUser(twilio, user, event.ConversationSid);
+
+            if (!selectedWorker) {
+                selectedWorker = getFirstAvailableWorker();
+            }
+
+            await twilio.conversations
+                .conversations(event.ConversationSid)
+                .participants
+                .create({ identity: selectedWorker })
+                .then(participant => console.log('Create agent participant: ', participant.sid))
+                .catch(e => console.log('Create agent participant: ', e));
+
+            return callback(null, event.ConversationSid);
+        }
+        
         callback(null);
+
 
     }
     else
         callback(null);
 }
+
+let getLastFrontLineUser = async (twilio, user, thisConversation) => {
+
+    //fetch participant conversations
+    let previousConversations = await twilio.conversations.participantConversations
+      .list({ address: user });
+  
+    if (!previousConversations) return;
+  
+    //find last conversation
+    let lastCreatedDate = new Date(-8640000000000000);  // min Date
+    let lastConversationSid;
+    previousConversations.forEach(p => {
+      if (p.conversationSid !== thisConversation && lastCreatedDate < p.conversationDateCreated) {
+        lastCreatedDate = p.conversationDateCreated;
+        lastConversationSid = p.conversationSid;
+      }
+    });
+  
+    console.log(lastConversationSid);
+  
+    //fetch frontline user from last conversation
+    let participants = await twilio.conversations.conversations(lastConversationSid).participants
+      .list();
+  
+    let lastFrontLineUser;
+    participants.some(p => {
+      lastFrontLineUser = p.identity;
+      return (p.identity);
+    });
+  
+    console.log(`lastFrontlineUser:${lastFrontLineUser}`);
+    return lastFrontLineUser;
+  }
+  
+  let getFirstAvailableWorker = () => {
+    // TODO implement this, taskrouter workers
+    return 'aandresdelvalle+frontline@twilio.com';
+    // return Math.random() == 0 ? 'tnascimento+frontline@twilio.com' : 'aandresdelvalle+frontline@twilio.com';
+  }
+
 
 /* 
 
