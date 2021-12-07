7e69d058f1bc2989f5e9e3883019e0070ff66205
Teresa N <teresa.raquel@gmail.com>
2021-12-07 12:39:25 +0000

    fixed event type for mananing participant data + error handling

--- serverless/frontline-service/functions/post-event.js
+++ serverless/frontline-service/functions/post-event.js
@@ -1,11 +1,8 @@
 exports.handler = async function (context, event, callback) {
-
-
+    console.log(`postEvent:`, event);
     const twilio = context.getTwilioClient();
     const response = new Twilio.Response();
     response.setStatusCode(200);
-
-    const customerNumber = event['MessagingBinding.Address'];
 
     let emptyAttributes = !event.Attributes || ('{}' == event.Attributes);
 
@@ -19,38 +16,43 @@ exports.handler = async function (context, event, callback) {
         callback(null, response);
     }
 
-    console.log(event);
-    //find customer in crm -- to add attributes to FrontLine application
+    if ('onParticipantAdded' == event.EventType) { //only update participant's data when they're added to the convo
+        const customerNumber = event['MessagingBinding.Address'];
 
-    const crm = require(Runtime.getFunctions()['crm'].path);
-    const crmCustomer = await crm.fetch(customerNumber, context.DB_URL)
-        .catch(error => callback(error));
-
-    //fetch participant
-    const participant = await twilio.conversations
-        .conversations(event.ConversationSid)
-        .participants
-        .get(event.ParticipantSid)
-        .fetch()
-        .catch(error => callback(error));
-
-    //save crm data to participant attributes
-    const participantAttributes = JSON.parse(participant.attributes);
-    const customerProperties = {
-        attributes: JSON.stringify({
-            ...participantAttributes,
-            avatar: participantAttributes.avatar || crmCustomer.avatar,
-            customer_id: participantAttributes.customer_id || crmCustomer.customer_id,
-            display_name: participantAttributes.display_name || crmCustomer.display_name
-        })
-    };
-
-    //update participant
-    if (participant.attributes !== customerProperties.attributes) {
-        await participant
-            .update(customerProperties)
+        //find customer in crm -- to add attributes to FrontLine application
+        const crm = require(Runtime.getFunctions()['crm'].path);
+        const crmCustomer = await crm.fetch(customerNumber, context)
             .catch(error => callback(error));
+
+        if (!crmCustomer) callback(`No crm record for ${customerNumber}`);
+
+        //fetch participant
+        const participant = await twilio.conversations
+            .conversations(event.ConversationSid)
+            .participants
+            .get(event.ParticipantSid)
+            .fetch()
+            .catch(error => callback(error));
+
+        if (!participant) callback('Participant not in the conversation');
+
+        //save crm data to participant attributes
+        const participantAttributes = JSON.parse(participant.attributes);
+        const customerProperties = {
+            attributes: JSON.stringify({
+                ...participantAttributes,
+                avatar: participantAttributes.avatar || crmCustomer.avatar,
+                customer_id: participantAttributes.customer_id || crmCustomer.customer_id,
+                display_name: participantAttributes.display_name || crmCustomer.display_name
+            })
+        };
+
+        //update participant
+        if (participant.attributes !== customerProperties.attributes) {
+            await participant
+                .update(customerProperties)
+                .catch(error => callback(error));
+        }
     }
-    
     callback(null, response);
 }
