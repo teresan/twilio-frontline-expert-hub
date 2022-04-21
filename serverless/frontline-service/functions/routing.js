b04bba3d1c4e2b34be85ccc317d12a9439e93c97
AnaCristina AndresDelValle <aandresdelvalle@twilio.com>
2022-04-21 17:53:09 +0200

    add search for copy

--- serverless/frontline-service/functions/routing.js
+++ serverless/frontline-service/functions/routing.js
@@ -1,11 +1,15 @@
 exports.handler = async function (context, event, callback) {
+  const bot = require(Runtime.getFunctions()['speakToBot'].path);
+
+  let language = await bot.getLanguage(context);
+  let intro = await bot.getCopy('intro', language, context);
 
   //no routing here, only bot welcome
   await context.getTwilioClient()
     .conversations
     .conversations(event['ConversationSid'])
     .messages
-    .create({ author: 'system', body: 'Welcome to the Expert Hub. How can I help you?' });
+    .create({ author: 'system', body: intro });
 
   return callback(null, 200);
 }
